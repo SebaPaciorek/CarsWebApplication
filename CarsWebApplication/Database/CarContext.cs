fae7538f42505de179330e3f88df6d3dd75a9bab
SPaciorek <paciorek8849@portal.ron.mil.pl>
2020-02-10 15:18:57 +0100

    first commit

---
+++ CarsWebApplication/Database/CarContext.cs
@@ -0,0 +1,27 @@
+﻿using CarsWebApplication.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsWebApplication.Database
+{
+    public class CarContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        public static readonly ILoggerFactory MyLoggerFactory
+            = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(MyLoggerFactory)
+               .UseSqlServer(
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CarsDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        }
+    }
+}
---
+++ CarsWebApplication/Model/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsWebApplication.Model
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int FuelEconomy { get; set; }
+        public int Cost { get; set; }
+    }
+}
