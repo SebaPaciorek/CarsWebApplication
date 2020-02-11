8c6615a5668239695c31828b5a7903f5fb82b4d1
SPaciorek <paciorek8849@portal.ron.mil.pl>
2020-02-11 08:57:09 +0100

    created car model

--- CarsWebApplication/Model/Car.cs
+++ CarsWebApplication/Model/Car.cs
@@ -7,6 +7,7 @@ namespace CarsWebApplication.Model
 {
     public class Car
     {
+        public int Id { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
         public string Color { get; set; }
