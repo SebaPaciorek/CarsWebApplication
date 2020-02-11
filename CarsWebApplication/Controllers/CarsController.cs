6deea3e85c0fd7bba548d1a77f9f8032f65755c7
SPaciorek <paciorek8849@portal.ron.mil.pl>
2020-02-11 09:33:41 +0100

    deleted comments

--- CarsWebApplication/Controllers/CarsController.cs
+++ CarsWebApplication/Controllers/CarsController.cs
@@ -43,8 +43,6 @@ namespace CarsWebApplication.Controllers
         }
 
         // PUT: api/Cars/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPut("{id}")]
         public async Task<IActionResult> PutCar(int id, Car car)
         {
@@ -75,8 +73,6 @@ namespace CarsWebApplication.Controllers
         }
 
         // POST: api/Cars
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPost]
         public async Task<ActionResult<Car>> PostCar(Car car)
         {
